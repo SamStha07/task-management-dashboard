9a08a2d0e9c718ababa4b4eb4c8e04752afefdbe
samstha07 <samshrestha85@gmail.com>
2026-01-10 16:05:33 +0545

    feat: setup for prettier, eslint, husky and lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +16,29 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      {
+        plugins: {
+          prettier: prettierPlugin
+        },
+        rules: {
+          ...prettierConfig.rules,
+          'prettier/prettier': 'error'
+        }
+      }
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        sourceType: 'module'
+      }
     },
-  },
-])
+    rules: {
+      // Add any custom rules here
+    }
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 
 function App() {
-  return <>Hello</>;
+  return <div>Hello</div>;
 }
-
 export default App;
