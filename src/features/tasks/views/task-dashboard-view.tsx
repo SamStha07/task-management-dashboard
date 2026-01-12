ec865a318ae03feccd3a465f5b203945c726a6f3
samstha07 <samshrestha85@gmail.com>
2026-01-12 15:33:00 +0545

    feat: add theme toggle feature

--- src/features/tasks/views/task-dashboard-view.tsx
+++ src/features/tasks/views/task-dashboard-view.tsx
@@ -1,10 +1,14 @@
 import ContainerLayout from '@/components/layouts/container-layout';
 import DashboardHeader from '../components/dashboard/dashboard-header';
 import TaskList from '../components/dashboard/task-list';
+import ModeToggle from '@/components/mode-toggle';
 
 export default function TaskDashboardView() {
   return (
     <ContainerLayout className="py-10">
+      <div className="flex justify-end">
+        <ModeToggle />
+      </div>
       <DashboardHeader />
       <TaskList />
     </ContainerLayout>
